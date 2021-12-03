67f22918372b92d910e245fde988d0854a4b0834
Vinícius Laureto <vinilaureto@gmail.com>
2021-12-03 01:02:50 -0300

    Create home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import Header from "../components/Header";
+import LoginForm from "../components/LoginForm";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <main className="center">
+        <LoginForm />
+      </main>
     </>
   );
 }
