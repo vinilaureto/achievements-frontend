1e688a7f6eea9094b7ce2e9accfa54741d4bbaff
Vinícius Laureto <vinilaureto@gmail.com>
2021-12-03 01:00:55 -0300

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,21 @@ import {
 } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import './styles/common.scss'
+import './styles/form.scss'
+import Signup from "./pages/Signup";
+import List from "./pages/List";
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />} exact/>
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Signup />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path={`/lista/:listId`} element={<List />} />
     </Routes>
   </BrowserRouter>
   );
