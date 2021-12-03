f1fc0ef5004abd04f3a392c1cab22036db296379
Vinícius Laureto <vinilaureto@gmail.com>
2021-12-03 01:03:13 -0300

    Create signup page

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import SignupForm from "../components/SignupForm";
+
+export default function Signup() {
+  return (
+    <>
+      <Header />
+      <main className="center">
+        <SignupForm />
+      </main>
+    </>
+  );
+}
