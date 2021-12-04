8e71e92d1e5eb32f38394f019bd6f27c207e4f64
Vinícius Laureto <vinilaureto@gmail.com>
2021-12-04 13:21:50 -0300

    Add conditional logoff button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,35 @@
 import "./style.scss";
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Header() {
-  const  navigate = useNavigate();
-  const email = localStorage.getItem('email')
+  const navigate = useNavigate();
+  const email = localStorage.getItem("email");
 
   function handleLogoff() {
-    localStorage.removeItem('email')
-    localStorage.removeItem('userId')
-    localStorage.removeItem('token')
-    navigate('/login')
+    localStorage.removeItem("email");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("token");
+    navigate("/login");
   }
 
   return (
     <header className="header">
       <Link to="/dashboard" className="link">
-      <h1 className="title">Kista</h1>
+        <h1 className="title">Kista</h1>
       </Link>
       <div className="controls">
         <span className="email">{email}</span>
-        <span className="separator"></span>
-        <button className="logoff" onClick={handleLogoff}>Sair</button>
+
+        {localStorage.getItem(email) ? (
+          <>
+            <span className="separator"></span>
+            <button className="logoff" onClick={handleLogoff}>
+              Sair
+            </button>
+          </>
+        ) : (
+          ""
+        )}
       </div>
     </header>
   );
