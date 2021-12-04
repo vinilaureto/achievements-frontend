25a218b9ae8e49ccad20408aca35a1980e5b2d19
Vinícius Laureto <vinilaureto@gmail.com>
2021-12-04 12:51:04 -0300

    Add context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import './styles/common.scss'
-import './styles/form.scss'
+import "./styles/common.scss";
+import "./styles/form.scss";
 import Signup from "./pages/Signup";
 import List from "./pages/List";
+import ContextProvider from "./lib/Context";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} exact/>
-      <Route path="/login" element={<Login />} />
-      <Route path="/cadastro" element={<Signup />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path={`/lista/:listId`} element={<List />} />
-    </Routes>
-  </BrowserRouter>
+    <ContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path={`/lista/:listId`} element={<List />} />
+        </Routes>
+      </BrowserRouter>
+    </ContextProvider>
   );
 }
 
