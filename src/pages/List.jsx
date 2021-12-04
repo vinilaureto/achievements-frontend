bb90eaa2f59264eee239c697797cc527401a0e76
Vinícius Laureto <vinilaureto@gmail.com>
2021-12-04 12:50:41 -0300

    Add achievement prop

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -3,18 +3,32 @@ import ListHeader from "../components/ListHeader";
 import Header from "../components/Header";
 import AchievementsList from "../components/AchievementsList";
 import CheckAuth from "../lib/Auth";
+import axios from 'axios'
+import { useCallback, useEffect, useState, useContext } from "react";
+import { Context } from "../lib/Context";
+import Footer from "../components/Footer";
 
-export default function List({ match }) {
-  const { listId } = useParams();
-  console.log(listId);
+export default function List() {
+  const { achievements, updateAchievementsList } = useContext(Context)
+  const listId = localStorage.getItem('listId')
+
+  const loadAchievements = useCallback(async () => {
+    updateAchievementsList(listId)
+  });
+
+  useEffect(() => {
+    loadAchievements();
+  }, []);
+
   return (
     <>
     <CheckAuth />
       <Header />
       <main className="top-padding container">
-        <ListHeader />
-        <AchievementsList />
+        <ListHeader achievements={achievements}/>
+        <AchievementsList achievements={achievements}/>
       </main>
+      <Footer />
     </>
   );
 }
