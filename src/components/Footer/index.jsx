7b73c6c07881cb5f9249e5319a47c0b6bffcc569
Vinícius Laureto <vinilaureto@gmail.com>
2021-12-04 12:51:56 -0300

    Create footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import './style.scss'
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p>Trabalho de Web IV - Vinícius Laureto - SC3008428 - IFSP São Carlos</p>
+        </footer>
+    )
+}
