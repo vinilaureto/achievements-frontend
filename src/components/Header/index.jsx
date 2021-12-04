575cde31b1b250b9e38370365d84fe99bc756307
Vinícius Laureto <vinilaureto@gmail.com>
2021-12-04 12:52:07 -0300

    Handle logoff

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,26 @@
 import "./style.scss";
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Header() {
+  const  navigate = useNavigate();
+  const email = localStorage.getItem('email')
+
+  function handleLogoff() {
+    localStorage.removeItem('email')
+    localStorage.removeItem('userId')
+    localStorage.removeItem('token')
+    navigate('/login')
+  }
+
   return (
     <header className="header">
       <Link to="/dashboard" className="link">
       <h1 className="title">Kista</h1>
       </Link>
       <div className="controls">
-        <span className="email">vinilaureto@gmail.com</span>
+        <span className="email">{email}</span>
         <span className="separator"></span>
-        <button className="logoff">Sair</button>
+        <button className="logoff" onClick={handleLogoff}>Sair</button>
       </div>
     </header>
   );
